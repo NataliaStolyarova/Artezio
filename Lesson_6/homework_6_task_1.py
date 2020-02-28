e058c272e966b3026747b0a7c88c91e4331bb614
NataliaStolyarova <semechkina.n@yandex.ru>
2020-02-28 13:06:13 +0300

    Add modified task 1

--- Lesson_6/homework_6_task_1.py
+++ Lesson_6/homework_6_task_1.py
@@ -5,13 +5,18 @@ class EvenIterator:
     """Receive a list of elements."""
     def __init__(self, lst):
         self.lst = lst
+        self.counter = 0
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        for i in range(0, len(self.lst), 2):
-            print(self.lst[i])
+        if self.counter < len(self.lst):
+            for i in range(0, len(self.lst), 2):
+                print(self.lst[i])
+            self.counter += 1
+        else:
+            raise StopIteration
 
 
 MY_LST = [1, 2, 3, 4, 5, 6]
