c2fc4d43effe7468b0168b0f037d1c8138575c82
NataliaStolyarova <semechkina.n@yandex.ru>
2020-02-28 12:30:47 +0300

    Add modified task2

--- Lesson_7/homework_7_task_2.py
+++ Lesson_7/homework_7_task_2.py
@@ -2,34 +2,42 @@
 from time import sleep, time
 
 
-def time_methods(method_name):
-    """The decorator measures the running time
-    of class methods."""
-
-    def wrapper(self, *args, **kwargs):
+def calc(function):
+    """Measures the running time."""
+    def wrapper(*args, **kwargs):
         current_time = time()
-        method_name(self, *args, **kwargs)
-        print(f'Время работы метода {method_name.__name__}:'
-              f' {time() - current_time}')
-
+        function(*args, **kwargs)
+        print(f'Время работы метода {function.__name__}:'
+              f' {(time() - current_time)*1000} ms')
     return wrapper
 
 
+def time_methods(*method_name):
+    """Sets attributes and passes an object
+    to the calc function."""
+    def class_obj(obj):
+        def wrapper(*args, **kwargs):
+            for name in method_name:
+                if hasattr(obj, name):
+                    setattr(obj, name, calc(getattr(obj, name)))
+            return obj(*args, **kwargs)
+
+        return wrapper
+    return class_obj
+
+
+@time_methods('inspect')
 class Spam:
     """The class decorated by decorator."""
 
-    def __init__(self, sec):
-        self.sec = sec
+    def __init__(self, s):
+        self.s = s
 
-    def __call__(self):
-        return self
-
-    @time_methods
     def inspect(self):
-        sleep(self.sec)
+        sleep(self.s)
 
     def func(self):
-        return self.sec
+        return self.s
 
 
 A = Spam(2)
