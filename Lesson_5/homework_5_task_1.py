68b63b46bf7e7744c4f04573884f79d88dadc1a5
NataliaStolyarova <semechkina.n@yandex.ru>
2020-02-28 21:54:48 +0300

    Add modified task 1

--- Lesson_5/homework_5_task_1.py
+++ Lesson_5/homework_5_task_1.py
@@ -1,58 +1,51 @@
-"""Arithmetic operations with complex numbers"""
+"""Arithmetic operations with complex numbers."""
 
 
 class ComplexNumbers:
-    """Class describing complex numbers"""
-    def __init__(self, real=None, imag=None):
-        self.real = real
-        self.imag = imag
+    """Class describing complex numbers
+    and realises arithmetic operations."""
 
-    def __str__(self):
+    def __init__(self, inp_str):
+        self.real = float(inp_str[0])
+        self.imag = float(inp_str[1])
+
+    def __add__(self, other):
+        real = self.real + other.real
+        imag = self.imag + other.imag
+        return ComplexNumbers([real, imag])
+
+    def __sub__(self, other):
+        real = self.real - other.real
+        imag = self.imag - other.imag
+        return ComplexNumbers([real, imag])
+
+    def __mul__(self, other):
+        real = self.real * other.real - self.imag * other.imag
+        imag = self.real * other.imag + self.imag * other.real
+        return ComplexNumbers([real, imag])
+
+    def __truediv__(self, other):
+        real = (self.real * other.real + self.imag * other.imag) /\
+               (other.real ** 2 + other.imag ** 2)
+        imag = (self.imag * other.real - self.real * other.imag) /\
+               (other.real ** 2 + other.imag ** 2)
+        return ComplexNumbers([real, imag])
+
+    def __abs__(self):
+        real = (self.real ** 2 + self.imag ** 2)**0.5
+        return ComplexNumbers([real, 0])
+
+    def __repr__(self):
         sign = '+' if self.imag >= 0 else ''
         return '{}{}{}i'.format("%.2f" % self.real, sign, "%.2f" % self.imag)
 
-    __repr__ = __str__
 
-    def __round__(self, positions):
-        print(ComplexNumbers(round(self.real, 2), round(self.imag, 2)))
-
-
-class Operations:
-    """Class that realises arithmetic operations"""
-    def __add__(self, a, b):
-        real = a.real + b.real
-        imag = a.imag + b.imag
-        print(ComplexNumbers(real, imag))
-
-    def __sub__(self, a, b):
-        real = a.real - b.real
-        imag = a.imag - b.imag
-        print(ComplexNumbers(real, imag))
-
-    def __mul__(self, a, b):
-        real = a.real * b.real - a.imag * b.imag
-        imag = a.imag * b.real + a.real * b.imag
-        print(ComplexNumbers(real, imag))
-
-    def __divmod__(self, a, b):
-        real = (a.real * b.real + a.imag * b.imag) / \
-               (b.real ** 2 + b.imag ** 2)
-        imag = (a.imag * b.real - a.real * b.imag) / \
-               (b.real ** 2 + b.imag ** 2)
-        print(ComplexNumbers(real, imag))
-
-    def mod_of_num(self, a):
-        mod = (a.real ** 2 + a.imag ** 2) ** 0.5
-        print(mod)
-
-
-x = ComplexNumbers(2, 1)
-y = ComplexNumbers(5, 6)
-
-op = Operations()
-op.__add__(x, y)
-op.__sub__(x, y)
-op.__mul__(x, y)
-op.__divmod__(x, y)
-op.mod_of_num(x)
-op.mod_of_num(y)
+if __name__ == "__main__":
+    C = ComplexNumbers(input("Enter x: ").split())
+    D = ComplexNumbers(input("Enter y: ").split())
+    print(C + D)
+    print(C - D)
+    print(C * D)
+    print(C / D)
+    print(abs(C))
+    print(abs(D))
